da8e5b363ec5da81b297b9be3fdeb6dfcacdb962
GolovninLev <golovnin_lev4444@mail.ru>
2023-09-07 20:48:59 +0300

    add comments to select_categories.php

--- public_html/back/select_categories.php
+++ public_html/back/select_categories.php
@@ -1,9 +1,14 @@
+<!-- Скрипт php для выгрузки из БД в массив всего содержимого таблиц с категориями оружия и характеристиках оружия -->
 <?php
 include "./connection.php";
+
+//  Подключение и запрос БД
 $link = mysqli_connect($host, $user, $password, $database) or die("error " . mysqli_error($link));
 $query="SELECT * FROM `Category`";
 $result=mysqli_query($link,$query);
 $array=[];
+
+// Запись результата ответа БД в массив
 if(mysqli_num_rows($result)!=0)
 {
     while($row=mysqli_fetch_array($result))
@@ -11,10 +16,15 @@ if(mysqli_num_rows($result)!=0)
         array_push($array, $row['Type']);
     }
 }
+
+
+// Подключение и запрос к БД
 $link = mysqli_connect($host, $user, $password, $database) or die("error " . mysqli_error($link));
 $query="SELECT * FROM `Weapons`";
 $result=mysqli_query($link,$query);
 $array1=[];
+
+// Запись результата ответа БД в массив и перенаправление пользователя на другую страницу в звсисимости от успешности запроса к БД
 if(mysqli_num_rows($result)!=0)
 {
     while($row=mysqli_fetch_array($result))
