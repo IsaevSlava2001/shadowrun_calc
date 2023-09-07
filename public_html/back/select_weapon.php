528488ca6deb96e2433d3540ab516312209d5e0c
GolovninLev <golovnin_lev4444@mail.ru>
2023-09-07 20:49:26 +0300

    add comments to select_weapon.php

--- public_html/back/select_weapon.php
+++ public_html/back/select_weapon.php
@@ -1,13 +1,19 @@
+<!-- Скрипт для выгрузки из БД информации про оружие с именем, выбранным пользвоателем -->
 <?php
+// Параметры подключения к БД
 $host = 'localhost';
 $database = 'q91976m8_calc';
 $user = 'q91976m8_calc';
 $password = "bnVhma1&";
 $weapon = $_GET['weapon'];
+
+//  Подключение и запрос БД
 $link = mysqli_connect($host, $user, $password, $database) or die("error " . mysqli_error($link));
 $query="SELECT `Damage`,`Type`,`Accuracy`,`AP`,`IsMelee` FROM `Weapons` WHERE `Name` = '$weapon'";
 $result=mysqli_query($link,$query);
 $array=[];
+
+// Запись в массив преобразованного ответа БД и отправка массива
 if(mysqli_num_rows($result)!=0)
 {
     while($row=mysqli_fetch_array($result))
@@ -20,5 +26,4 @@ else
 {
     echo "no such weapon";
 }
-
 ?>
