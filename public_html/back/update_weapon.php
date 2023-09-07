8dd37cd0d43b4d9d80d068335e64a606b0852e2a
GolovninLev <golovnin_lev4444@mail.ru>
2023-09-07 20:49:36 +0300

    add comments to update_weapon.php

--- public_html/back/update_weapon.php
+++ public_html/back/update_weapon.php
@@ -1,25 +1,34 @@
+<!-- Скрипт поиска введённого пользователем оружия среди самых обновлённых данных БД. (В случае, если оружие не было нейдено) -->
 <?php
+// Параметры подключения к БД
 $host = 'localhost';
 $database = 'q91976m8_calc';
 $user = 'q91976m8_calc';
 $password = "bnVhma1&";
 $cat = $_GET['categ'];
+
 if($cat!="no such weapon")
 {
+    //  Подключение и запрос БД
     $link = mysqli_connect($host, $user, $password, $database) or die("error " . mysqli_error($link));
     $query="SELECT `ID` FROM `Category` WHERE `Type` = '$cat'";
     $result=mysqli_query($link,$query);
-    $f;
     $array=[];
+
+    $f;
     if(mysqli_num_rows($result)!=0)
     {
         while($row=mysqli_fetch_array($result))
         {
             $f=$row['ID'];
         }
+
+        // Запрос БД
         $query="SELECT `Name` FROM `Weapons` WHERE `Category` = $f";
         $result=mysqli_query($link,$query);
         $array1=[];
+
+        // Заполнение массива и его отправка
         if(mysqli_num_rows($result)!=0)
         {
             while($row=mysqli_fetch_array($result))
@@ -39,3 +48,4 @@ if($cat!="no such weapon")
     }
 }
 ?>
+
