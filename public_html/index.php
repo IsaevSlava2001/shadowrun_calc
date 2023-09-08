5f0d2aa71f14795f644b188d0b657e999112c672
GolovninLev <golovnin_lev4444@mail.ru>
2023-09-08 10:59:37 +0300

    added styles, alignment and adaptive

--- public_html/index.php
+++ public_html/index.php
@@ -9,13 +9,15 @@
 </head>
 
 
-<body style="background-image:url('pictures/back2.jpg'); height: 994px; width: 1500px; display: middle ;">
+<body style="background: url('pictures/back2.jpg') 0 0">
 
+    
     <?php
     require_once("back/select_categories.php");
     ?>
 
-    <div style="width: 184px; position: relative; background-color: #fc990487;" class="base">
+    <div style="min-width: 905px; max-width: 905px; margin: 0 auto;">
+        <div style="width: 200px; position: relative; background-color: #fc990487;" class="base">
 	Category<br>
 		<select name="Select1" id="updcat">
 		<option value="">--Please choose an option--
@@ -27,7 +29,7 @@
 			?>
 		</select>
 	</div>
-	<div style="width: 184px; position: relative; left: 0px; top: 0px; background-color: #fc990487;" class="base">
+	<div style="width: 200px; position: relative; left: 0px; top: 0px; background-color: #fc990487;" class="base">
 	Weapon<br>
 		<select name="Select2" id="updweapon">
 		<option value="">--Please choose an option--
@@ -39,62 +41,62 @@
 			?>
 		</select>
 	</div>
-	<div style="width: 84px; height: 38px" class="base">
+	<div style="width: 90px; height: 38px" class="base">
 	Damage<br>
 	<label id="Label1"></label></div>
-	<div style="width: 92px; position: relative; left: 92px; top: -38px;" class="base">
+	<div style="width: 100px; height: 38px; position: relative; left: 100px; top: -48px;" class="base">
 	AP<br>
 	<label id="Label2"></label></div>
-	<div style="width: 184px; position: relative; left: 0px; top: -28px; background-color: #ff000046;" class="base">
+	<div style="width: 200px; position: relative; left: 0px; top: -50px; background-color: #ff000046;" class="base">
 	Strength<br>
 	<input name="Text1" type="number" style="width: 172px" max="20" min="1">
 	</div>
 	
 	
-	<div style="width: 130px; position: relative; left: 200px; top: -200px; height: 118px;" class="base">
+	<div style="width: 130px; position: relative; left: 220px; top: -238px; height: 118px; background-color: #ffd90071;" class="base">
 		<br>Attack<br>
 		<label id="Label3" style="position: absolute; z-index: 1; top: 46px; left: 56px;"></label>
 		<img id="atackcount" alt="" src="pictures/dice.jpg" height="23" style="position: relative; left: 0px; top: 40px; z-index: 1" width="25">
 	</div>
-	<div style="width: 130px; position: relative; left: 350px; top: -328px; height: 117px;" class="base">
+	<div style="width: 130px; position: relative; left: 360px; top: -366px; height: 117px;" class="base">
 		<br>Accuracy<br>
 		<label id="Label4" style="position: absolute; z-index: 1; top: 46px; left: 56px;"></label>
 	</div>
-	<div style="width: 261px; position: relative; left: 200px; top: -315px; height: 42px; background-color: #ff000046;" class="base">
+	<div style="width: 270px; position: relative; left: 220px; top: -355px; height: 42px; background-color: #ff00002e;" class="base">
 		Attack dicepool<br>
 		<input name="Text2" type="number" min="0"><br>
 	</div>
-	<div style="width: 200px; position: relative; left: 500px; top: -505px; height: 116px;" class="base">
+	<div style="width: 200px; position: relative; left: 510px; top: -545px; height: 116px; background-color: #ffd90071;" class="base">
 		<br>Dodge<br>
 		<label id="Label5" style="position: absolute; z-index: 1; top: 46px; left: 56px;"></label>
 		<img id="dodgecount" alt="" src="pictures/dice.jpg" height="23" style="position: relative; left: 0px; top: 40px; z-index: 1" width="25">
 	</div>
-	<div style="width: 200px; position: relative; left: 500px; top: -490px; height: 39px; background-color: #1eff0046;" class="base">
+	<div style="width: 200px; position: relative; left: 510px; top: -532px; height: 39px; background-color: #1eff0046;" class="base">
 	Dodge dicepool<br>
 		<input name="Text3" type="number" min="0"><br>
 	</div>
-	<div style="width: 130px; position: relative; left: 720px; top: -680px; height: 135px;" class="base">
+	<div style="width: 130px; position: relative; left: 720px; top: -720px; height: 115px;" class="base">
 		<br>Net hits<br>
 		<label id="Label6" style="position: absolute; z-index: 1; top: 46px; left: 56px;"></label>
 	</div>
-	<div style="width: 130px; position: relative; left: 200px; top: -610px; height: 153px;" class="base">
+	<div style="width: 130px; position: relative; left: 220px; top: -610px; height: 153px;" class="base">
 		<br>Damage<br>
 		<label id="Label7" style="position: absolute; z-index: 1; top: 46px; left: 56px;"></label>
 	</div>
-	<div style="width: 130px; position: relative; left: 350px; top: -775px; height: 153px;" class="base">
+	<div style="width: 130px; position: relative; left: 360px; top: -773px; height: 153px;" class="base">
 		<br>Net hits<br>
 		<label id="Label8" style="position: absolute; z-index: 1; top: 46px; left: 56px;"></label>
 	</div>
-	<div style="width: 200px; position: relative; left: 500px; top: -935px; height: 100px;" class="base">
+	<div style="width: 200px; position: relative; left: 510px; top: -937px; height: 100px; background-color: #ffd90071;" class="base">
 		<br>Resist<br>
 		<label id="Label9" style="position: absolute; z-index: 1; top: 46px; left: 56px;"></label>
 		<img id="resistcount" alt="" src="pictures/dice.jpg" height="23" style="position: relative; left: 0px; top: 40px; z-index: 1" width="25">
 	</div>
-	<div style="width: 200px; position: relative; left: 500px; top: -927px; height: 42px; background-color: #1eff0046;" class="base">
+	<div style="width: 200px; position: relative; left: 510px; top: -927px; height: 42px; background-color: #1eff0046;" class="base">
 		Resist dicepool<br>
 		<input name="Text4" type="number" min="0"><br>
 	</div>
-	<div style="width: 167px; position: relative; left: 720px; top: -1100px; height: 153px;" class="base">
+	<div style="width: 170px; position: relative; left: 720px; top: -1100px; height: 162px;" class="base">
 		<p style="position: relative; left: 30px; top: 5px; width: 50px;">Damage taken</p><br>
 		<span lang="ru">
 		<label id="Label10" style="position: absolute; z-index: 1; top: 46px; left: 56px;">
@@ -108,6 +110,8 @@
 	<div>
 		<input type="hidden" id="type"></input>
 	</div>
+    </div>
+    
 </body>
 
 
